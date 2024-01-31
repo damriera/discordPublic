5b55ffa8ddf7a564484d379c7f18fca1871d787c
damriera <152306561+damriera@users.noreply.github.com>
2024-01-31 15:11:47 +0100

    Add files via upload

--- discordTracker.py
+++ discordTracker.py
@@ -1,4 +1,6 @@
 import requests
+import json
+import time
 
 """
 This program takes messages from a discord channel and keep tracks of new messages by printing it onto the user's terminal.
@@ -12,7 +14,7 @@ def retrieve_messages(channelid):
     """
     
     headers = {
-        'authorization': '(put channel authorization here)',
+        'authorization': 'put your channel authorization here',
         'X-RateLimit-Limit': '45',
         'X-RateLimit-Reset-After': '1'
     }
@@ -33,35 +35,45 @@ def print_messages(channelid, jsonn):
     """
     Takes the messages from the same channel as the channel used for jsonn and puts it in checker 
     if the top message id from checker isn't the same as the top message id from jsonn, then a new message was sent
-    if a new message was sent, prints it in the terminal with print_new_messages(), then send back checker.
+    if a new message was sent, prints it in the terminal, then send back checker.
     """
 
     headers = {
-        'authorization': '(put channel authorization here)',
+        'authorization': 'put your channel authorization here',
         'X-RateLimit-Limit': '45',
         'X-RateLimit-Reset-After': '1'
     }
 
     r = requests.get(f'https://discord.com/api/v9/channels/{channelid}/messages', headers=headers)
     checker = json.loads(r.text)
-    # Get the top message ID from jsonn
-    id1 = jsonn[0]['id'] if jsonn else None
-    
-    # Get the top message ID from checker
-    id2 = checker[0]['id'] if checker else None
-    
-    # Compare top message IDs
+    k = 0
+    id1 = 0
+    id2 = 0
+    for value in checker:
+        if k == 0:
+            id1 = value['id']
+            k += 1
+        else:
+            k = 0
+            break
+    for value in jsonn:
+        if k == 0:
+            id2 = value['id'] 
+            k += 1
+        else:
+            k=0
+            break
     if id1 != id2:
-        print_new_messages(checker)
-    
+        for value in checker:
+            if k == 0:
+                print(f"Message from {value['author']['username']}:")      
+                print(value['content'], "\n") 
+                k += 1
+            else:
+                break
     return checker
 
-def print_new_messages(messages):
 
-	"""
-	used to print new messages.
-	"""
 
-    for value in messages:
-        print(f"Message from {value['author']['username']}:")
-        print(value['content'], "\n")
+
+retrieve_messages("put channel id here")
